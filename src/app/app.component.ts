5490f03621515ca436c9c09d2038f5bbb500ea69
davidaguirreg <49161379+davidaguirreg@users.noreply.github.com>
2023-11-01 21:09:39 -0500

    [ADD] [DA] Add new comments and list of comments
    Html structure for new comment and list comments were added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from './interfaces/user.interface';
 import { UserService } from './services/user.service';
+import { Comment } from './interfaces/comment.interface';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +15,37 @@ export class AppComponent {
     profileImage: 'User'
   }
 
+  public comment : Comment = {
+    user: {...this.newUser},
+    message: 'I love Aval Buro'
+  }
+
+  public commentsList : Comment[] = [
+    {
+      user: {
+        name: 'David Aguirre',
+        profileImage: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+      },
+      message: 'Hey What is up folk'
+    },
+    {
+      user: {
+        name: 'Carmine 6899',
+        profileImage: 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp'
+      },
+      message: 'Hey Why did u come to this place?'
+    }
+  ]
   constructor(
     private userService: UserService
   ){}
 
-  saveNewUser(user:User): void {
+  saveNewUser( user : User ) : void {
     this.userService.addNewUser(user);
+  }
+
+  saveUserComment( newComment : Comment ) : void {
+
   }
 
 }
